3c29390b6dac7e10e59dd1e111e93ac179625053
Fredrik <fredrik.burmester@gmail.com>
2020-11-27 09:13:20 +0000

    improve saliency score with svds and abs()

--- saliency/saliency_score.py
+++ saliency/saliency_score.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.sparse import csc_matrix
+from scipy.sparse.linalg import svds, eigs
 
 def saliency_score(A):
-    u, s, vh = np.linalg.svd(A, full_matrices=True)
-    max_values = []
+    # u, s, vh = np.linalg.svd(A, 1)
+    u, s, vt = svds(A, k=1, which='LM')
 
-    for index in range(1,u.shape[1]):
-        col = u[index]
-        value = max(col)
-        max_values.append(value)
-
-    plt.plot(max_values)
+    # u = u[0]
+    
+    plt.plot(abs(u))
     plt.savefig('./plot.png')
+    u = abs(u)
+    return(np.argsort(-u.T))
