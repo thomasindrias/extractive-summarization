ecab12af1efaf73ffe1f009350f3555e2faaada5
Fredrik <fredrik.burmester@gmail.com>
2020-12-07 09:10:23 +0000

    have saliency score return vt

--- saliency/saliency_score.py
+++ saliency/saliency_score.py
@@ -8,8 +8,7 @@ def saliency_score(A):
     u, s, vt = svds(A, k=1, which='LM')
 
     # u = u[0]
-    
-    plt.plot(abs(u))
-    plt.savefig('./plot.png')
+
     u = abs(u)
-    return(np.argsort(-u.T))
+
+    return(u, np.argsort(-u.T), np.argsort(vt))
