c6a59f8c060293b427349f54ab0ec7d6dd380c23
Fredrik <fredrik.burmester@gmail.com>
2020-12-08 11:19:16 +0000

    added create graph function

--- main.py
+++ main.py
@@ -12,8 +12,36 @@ import matplotlib.pyplot as plt
 lemmatizer = WordNetLemmatizer()
 stop_words = stopwords.words('english')
 
+paths = ['data/wikipedia_text_mining.txt', 'data/the_guardian_vaccine_article.txt', 'data/weirdest_computer.txt']
 
 # Read and parse a text document, in this case an article about the Covid-19 vaccine
+
+def create_graph():
+    fig, ax = plt.subplots(2, 3, figsize=(20,10), constrained_layout=True)
+    fig.suptitle('Key word and key sentence scores', fontsize=16)
+    
+    for index, path in enumerate(paths):
+        print(f'Running algorithm on {path}\n')
+        u = None
+        u, C = main(path)
+        
+        ax[0, index].plot(u)
+        ax[0, index].set_xlabel('Terms', fontsize = 12)
+        ax[0, index].set_ylabel('Score', fontsize = 12)
+        ax[1, index].plot(C)
+        ax[1, index].set_xlabel('Terms', fontsize = 12)  
+        ax[1, index].set_ylabel('Score', fontsize = 12)  
+
+    ax[0, 0].title.set_text('Saliency Score DB1')
+    ax[0, 1].title.set_text('Saliency Score DB2')
+    ax[0, 2].title.set_text('Saliency Score DB3')
+
+    ax[1, 0].title.set_text('Rank-k approximation DB1')
+    ax[1, 1].title.set_text('Rank-k approximation DB2')
+    ax[1, 2].title.set_text('Rank-k approximation DB3')
+
+    plt.savefig('./plot.png')
+
 
 def main(path):
     """
@@ -44,7 +72,7 @@ def main(path):
 
     print("\n========= Key terms ============\n")
 
-    print(doc.terms[key_terms][0][:10])
+    print(doc.terms[key_terms][:10])
     print("\n")
     
     for (i, sentence) in enumerate(key_sencences_sorted[:5]):
@@ -57,10 +85,9 @@ def main(path):
     
     return u, C
 
-
-paths = ['data/wikipedia_text_mining.txt', 'data/the_guardian_vaccine_article.txt', 'data/weirdest_computer.txt']
-
 if __name__ == '__main__':
     for path in paths:
         print(f'Running algorithm on {path}\n')
         main(path)
+
+    create_graph()
